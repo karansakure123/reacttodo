dd538766546d0ccdd885e5248e4d0ea187f31870
karansakure123git <sakurekaran12@example.com>
2025-12-29 13:54:39 +0530

    ui changes of react to test codepipeline cicd

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,12 +14,12 @@ const Home = () => {
   return (
     <>
     <h1 className="p-2 m-2 text-center text-3xl mt-3 text-pink-100">Welcome here</h1>
-     <div className="max-w-7xl mx-auto p-4">
+     <div className="max-w-7xl mx-auto p-4 bg-black ">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {images.map((img, index) => (
           <div
             key={index}
-            className="bg-black rounded-xl shadow-md overflow-hidden hover:shadow-xl transition"
+            className="bg-black text-white rounded-xl shadow-md overflow-hidden hover:shadow-xl transition"
           >
             <img
               src={img}
@@ -39,4 +39,5 @@ const Home = () => {
     </>
   )
 }
+
 export default Home
