ccf2de2618fc1d14d1ffd0afec4159c8208ca748
karansakure123git <sakurekaran12@example.com>
2025-12-29 13:49:23 +0530

    updated ui testing cicd using codepipeline react

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,13 +13,13 @@ const Home = () => {
   ];
   return (
     <>
-    <h1 className="p-2 m-2 text-center text-3xl mt-3 text-blue-950">Welcome here</h1>
+    <h1 className="p-2 m-2 text-center text-3xl mt-3 text-pink-100">Welcome here</h1>
      <div className="max-w-7xl mx-auto p-4">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {images.map((img, index) => (
           <div
             key={index}
-            className="bg-white rounded-xl shadow-md overflow-hidden hover:shadow-xl transition"
+            className="bg-black rounded-xl shadow-md overflow-hidden hover:shadow-xl transition"
           >
             <img
               src={img}
@@ -39,5 +39,4 @@ const Home = () => {
     </>
   )
 }
-
 export default Home
